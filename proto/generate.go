871a104e9cb69d1f18291f0219e4a6f3cb9c6f37
Gennadii Muzykantov <46723083+muzykantov@users.noreply.github.com>
2024-06-05 16:26:17 +0300

    Rename config.proto to foundation_config.proto to prevent proto name conflict (#57)
    Signed-off-by: Gennadii Muzykantov <gennadii@muzykantov.me>

--- proto/generate.go
+++ proto/generate.go
@@ -5,4 +5,4 @@ package proto
 //go:generate protoc -I=. --go_out=paths=source_relative:. locks.proto
 
 // Chaincode configuration
-//go:generate protoc -I=. -I=./validate --go_out=paths=source_relative:. --validate_out=lang=go,paths=source_relative:. config.proto
+//go:generate protoc -I=. -I=./validate --go_out=paths=source_relative:. --validate_out=lang=go,paths=source_relative:. foundation_config.proto
