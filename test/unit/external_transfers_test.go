a1f7926fd288728ab555ef45f1cf25364ab35e4c
Dmitry Atapin <73831539+AtapinDmitry@users.noreply.github.com>
2024-12-22 18:57:20 +0300

    External transfer test (#203)
    * external transfers test migrated to MockStub usage
    
    Signed-off-by: Dmitry Atapin <dmitry.atapin.83@gmail.com>
    
    * goimports fix
    
    Signed-off-by: Dmitry Atapin <dmitry.atapin.83@gmail.com>
    
    ---------
    
    Signed-off-by: Dmitry Atapin <dmitry.atapin.83@gmail.com>

--- test/unit/external_transfers_test.go
+++ test/unit/external_transfers_test.go
@@ -2,231 +2,179 @@ package unit
 
 import (
 	"encoding/json"
+	"net/http"
 	"testing"
 	"time"
 
 	"github.com/anoideaopen/foundation/core"
 	"github.com/anoideaopen/foundation/core/balance"
-	"github.com/anoideaopen/foundation/mock"
+	"github.com/anoideaopen/foundation/core/types/big"
+	"github.com/anoideaopen/foundation/mocks"
+	"github.com/anoideaopen/foundation/mocks/mockstub"
 	"github.com/anoideaopen/foundation/proto"
+	pb "github.com/golang/protobuf/proto"
+	"github.com/hyperledger/fabric-protos-go/peer"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
-func TestAdminTransferBalance(t *testing.T) {
-	ledger := mock.NewLedger(t)
-	owner := ledger.NewWallet()
+func TestExternalTransfers(t *testing.T) {
+	testCollection := []struct {
+		name             string
+		amountToTransfer string
+		invokeFunc       func(cc *core.Chaincode, mockStub *mockstub.MockStub, owner, user *mocks.UserFoundation, parameters ...string) peer.Response
+		checkResultFunc  func(t *testing.T, mockStub *mockstub.MockStub, resp peer.Response, user1BalanceKey, user2BalanceKey string)
+	}{
+		{
+			name:             "happy path",
+			amountToTransfer: "600",
+			invokeFunc: func(cc *core.Chaincode, mockStub *mockstub.MockStub, owner, user *mocks.UserFoundation, parameters ...string) peer.Response {
+				_, resp := mockStub.TxInvokeChaincodeSigned(cc, "transferBalance", owner, "", "", "", parameters...)
+				return resp
+			},
+			checkResultFunc: func(t *testing.T, mockStub *mockstub.MockStub, resp peer.Response, user1BalanceKey, user2BalanceKey string) {
+				balance1Checked := false
+				balance2Checked := false
+				for i := 0; i < mockStub.PutStateCallCount(); i++ {
+					putStateKey, value := mockStub.PutStateArgsForCall(i)
 
-	ccConfig := makeBaseTokenConfig("CC Token", "CC", 8,
-		owner.Address(), "", "", owner.Address(), nil)
+					if putStateKey == user1BalanceKey {
+						bal := new(big.Int).SetBytes(value)
+						require.Equal(t, 0, bal.Cmp(big.NewInt(400)))
+						balance1Checked = true
+					}
 
-	initMsg := ledger.NewCC("cc", &CustomToken{}, ccConfig)
-	require.Empty(t, initMsg)
-
-	user1 := ledger.NewWallet()
-	user2 := ledger.NewWallet()
-	user1.AddBalance("cc", 1000)
-	user2.AddBalance("cc", 500)
-
-	transferRequest := &proto.TransferRequest{
-		RequestId:       "",
-		Basis:           proto.TransferBasis_TRANSFER_BASIS_INHERITANCE,
-		AdministratorId: owner.Address(),
-		DocumentType:    proto.DocumentType_DOCUMENT_TYPE_INHERITANCE,
-		DocumentNumber:  "1",
-		DocumentDate:    timestamppb.New(time.Now()),
-		DocumentHashes: []string{
-			"hash1", "hash2",
+					if putStateKey == user2BalanceKey {
+						bal := new(big.Int).SetBytes(value)
+						require.Equal(t, 0, bal.Cmp(big.NewInt(1100)))
+						balance2Checked = true
+					}
+				}
+				require.True(t, balance1Checked && balance2Checked)
+			},
 		},
-		FromAddress:    user1.Address(),
-		ToAddress:      user2.Address(),
-		Token:          "",
-		Amount:         "600",
-		Reason:         "test transfer",
-		BalanceType:    proto.BalanceType_BALANCE_TYPE_TOKEN,
-		AdditionalInfo: nil,
+		{
+			name:             "[negative] unauthorized invoke",
+			amountToTransfer: "600",
+			invokeFunc: func(cc *core.Chaincode, mockStub *mockstub.MockStub, owner, user *mocks.UserFoundation, parameters ...string) peer.Response {
+				_, resp := mockStub.TxInvokeChaincodeSigned(cc, "transferBalance", user, "", "", "", parameters...)
+				return resp
+			},
+			checkResultFunc: func(t *testing.T, mockStub *mockstub.MockStub, resp peer.Response, user1BalanceKey, user2BalanceKey string) {
+				payload := &proto.BatchResponse{}
+
+				err := pb.Unmarshal(resp.Payload, payload)
+				require.NoError(t, err)
+				require.Equal(t, core.ErrUnauthorisedNotAdmin.Error(), payload.TxResponses[0].Error.Error)
+			},
+		},
+		{
+			name:             "[negative] insufficient funds",
+			amountToTransfer: "1100",
+			invokeFunc: func(cc *core.Chaincode, mockStub *mockstub.MockStub, owner, user *mocks.UserFoundation, parameters ...string) peer.Response {
+				_, resp := mockStub.TxInvokeChaincodeSigned(cc, "transferBalance", owner, "", "", "", parameters...)
+				return resp
+			},
+			checkResultFunc: func(t *testing.T, mockStub *mockstub.MockStub, resp peer.Response, user1BalanceKey, user2BalanceKey string) {
+				payload := &proto.BatchResponse{}
+
+				err := pb.Unmarshal(resp.Payload, payload)
+				require.NoError(t, err)
+				require.Equal(t, balance.ErrInsufficientBalance.Error(), payload.TxResponses[0].Error.Error)
+			},
+		},
+		{
+			name:             "[negative] negative amount transfer",
+			amountToTransfer: "-100",
+			invokeFunc: func(cc *core.Chaincode, mockStub *mockstub.MockStub, owner, user *mocks.UserFoundation, parameters ...string) peer.Response {
+				_, resp := mockStub.TxInvokeChaincodeSigned(cc, "transferBalance", owner, "", "", "", parameters...)
+				return resp
+			},
+			checkResultFunc: func(t *testing.T, mockStub *mockstub.MockStub, resp peer.Response, user1BalanceKey, user2BalanceKey string) {
+				payload := &proto.BatchResponse{}
+
+				err := pb.Unmarshal(resp.Payload, payload)
+				require.NoError(t, err)
+				require.Equal(t, core.ErrAmountMustBeGreaterThanZero.Error(), payload.TxResponses[0].Error.Error)
+			},
+		},
+		{
+			name:             "[negative] zero amount transfer",
+			amountToTransfer: "0",
+			invokeFunc: func(cc *core.Chaincode, mockStub *mockstub.MockStub, owner, user *mocks.UserFoundation, parameters ...string) peer.Response {
+				_, resp := mockStub.TxInvokeChaincodeSigned(cc, "transferBalance", owner, "", "", "", parameters...)
+				return resp
+			},
+			checkResultFunc: func(t *testing.T, mockStub *mockstub.MockStub, resp peer.Response, user1BalanceKey, user2BalanceKey string) {
+				payload := &proto.BatchResponse{}
+
+				err := pb.Unmarshal(resp.Payload, payload)
+				require.NoError(t, err)
+				require.Equal(t, core.ErrAmountMustBeGreaterThanZero.Error(), payload.TxResponses[0].Error.Error)
+			},
+		},
 	}
 
-	data, err := json.Marshal(transferRequest)
-	require.NoError(t, err)
+	for _, test := range testCollection {
+		t.Run(test.name, func(t *testing.T) {
+			mockStub := mockstub.NewMockStub(t)
 
-	t.Log(string(data))
+			owner, err := mocks.NewUserFoundation(proto.KeyType_ed25519)
+			require.NoError(t, err)
 
-	err = owner.RawSignedInvokeWithErrorReturned("cc", "transferBalance", string(data))
-	require.NoError(t, err)
+			ccConfig := makeBaseTokenConfig("CC Token", "CC", 8,
+				owner.AddressBase58Check, "", "", owner.AddressBase58Check, nil)
 
-	user1.BalanceShouldBe("cc", 400)
-	user2.BalanceShouldBe("cc", 1100)
-}
+			cc, err := core.NewCC(&CustomToken{})
+			require.NoError(t, err)
 
-func TestNotAdminFailedTransfer(t *testing.T) {
-	ledger := mock.NewLedger(t)
-	owner := ledger.NewWallet()
+			mockStub.SetConfig(ccConfig)
 
-	ccConfig := makeBaseTokenConfig("CC Token", "CC", 8,
-		owner.Address(), "", "", owner.Address(), nil)
-	initMsg := ledger.NewCC("cc", &CustomToken{}, ccConfig)
-	require.Empty(t, initMsg)
+			user1, err := mocks.NewUserFoundation(proto.KeyType_ed25519)
+			require.NoError(t, err)
 
-	user1 := ledger.NewWallet()
-	user2 := ledger.NewWallet()
-	user1.AddBalance("cc", 1000)
-	user2.AddBalance("cc", 500)
+			user2, err := mocks.NewUserFoundation(proto.KeyType_ed25519)
+			require.NoError(t, err)
 
-	transferRequest := &proto.TransferRequest{
-		RequestId:       "",
-		Basis:           proto.TransferBasis_TRANSFER_BASIS_INHERITANCE,
-		AdministratorId: owner.Address(),
-		DocumentType:    proto.DocumentType_DOCUMENT_TYPE_INHERITANCE,
-		DocumentNumber:  "1",
-		DocumentDate:    timestamppb.New(time.Now()),
-		DocumentHashes: []string{
-			"hash1", "hash2",
-		},
-		FromAddress:    user1.Address(),
-		ToAddress:      user2.Address(),
-		Token:          "",
-		Amount:         "600",
-		Reason:         "test transfer",
-		BalanceType:    proto.BalanceType_BALANCE_TYPE_TOKEN,
-		AdditionalInfo: nil,
+			// adding balances
+			balanceUser1Key, err := mockStub.CreateCompositeKeyStub(balance.BalanceTypeToken.String(), []string{user1.AddressBase58Check})
+			require.NoError(t, err)
+
+			mockStub.GetStateCallsMap[balanceUser1Key] = big.NewInt(1000).Bytes()
+
+			balanceUser2Key, err := mockStub.CreateCompositeKeyStub(balance.BalanceTypeToken.String(), []string{user2.AddressBase58Check})
+			require.NoError(t, err)
+
+			mockStub.GetStateCallsMap[balanceUser2Key] = big.NewInt(500).Bytes()
+
+			// preparing transfer request
+			transferRequest := &proto.TransferRequest{
+				RequestId:       "",
+				Basis:           proto.TransferBasis_TRANSFER_BASIS_INHERITANCE,
+				AdministratorId: owner.AddressBase58Check,
+				DocumentType:    proto.DocumentType_DOCUMENT_TYPE_INHERITANCE,
+				DocumentNumber:  "1",
+				DocumentDate:    timestamppb.New(time.Now()),
+				DocumentHashes: []string{
+					"hash1", "hash2",
+				},
+				FromAddress:    user1.AddressBase58Check,
+				ToAddress:      user2.AddressBase58Check,
+				Token:          "",
+				Amount:         test.amountToTransfer,
+				Reason:         "test transfer",
+				BalanceType:    proto.BalanceType_BALANCE_TYPE_TOKEN,
+				AdditionalInfo: nil,
+			}
+
+			data, err := json.Marshal(transferRequest)
+			require.NoError(t, err)
+
+			resp := test.invokeFunc(cc, mockStub, owner, user1, []string{string(data)}...)
+			require.Equal(t, int32(http.StatusOK), resp.GetStatus())
+			require.Empty(t, resp.GetMessage())
+
+			test.checkResultFunc(t, mockStub, resp, balanceUser1Key, balanceUser2Key)
+		})
 	}
-
-	data, err := json.Marshal(transferRequest)
-	require.NoError(t, err)
-
-	err = user1.RawSignedInvokeWithErrorReturned("cc", "transferBalance", string(data))
-	require.EqualError(t, err, core.ErrUnauthorisedNotAdmin.Error())
-
-	user1.BalanceShouldBe("cc", 1000)
-	user2.BalanceShouldBe("cc", 500)
-}
-
-func TestInsufficientFundsTransfer(t *testing.T) {
-	ledger := mock.NewLedger(t)
-	owner := ledger.NewWallet()
-
-	ccConfig := makeBaseTokenConfig("CC Token", "CC", 8,
-		owner.Address(), "", "", owner.Address(), nil)
-	initMsg := ledger.NewCC("cc", &CustomToken{}, ccConfig)
-	require.Empty(t, initMsg)
-
-	user1 := ledger.NewWallet()
-	user2 := ledger.NewWallet()
-	user1.AddBalance("cc", 500)
-	user2.AddBalance("cc", 500)
-
-	transferRequest := &proto.TransferRequest{
-		RequestId:       "",
-		Basis:           proto.TransferBasis_TRANSFER_BASIS_INHERITANCE,
-		AdministratorId: owner.Address(),
-		DocumentType:    proto.DocumentType_DOCUMENT_TYPE_INHERITANCE,
-		DocumentNumber:  "1",
-		DocumentDate:    timestamppb.New(time.Now()),
-		DocumentHashes: []string{
-			"hash1", "hash2",
-		},
-		FromAddress:    user1.Address(),
-		ToAddress:      user2.Address(),
-		Token:          "",
-		Amount:         "600",
-		Reason:         "test transfer",
-		BalanceType:    proto.BalanceType_BALANCE_TYPE_TOKEN,
-		AdditionalInfo: nil,
-	}
-
-	data, err := json.Marshal(transferRequest)
-	require.NoError(t, err)
-
-	err = owner.RawSignedInvokeWithErrorReturned("cc", "transferBalance", string(data))
-	require.EqualError(t, err, balance.ErrInsufficientBalance.Error())
-
-	user1.BalanceShouldBe("cc", 500)
-	user2.BalanceShouldBe("cc", 500)
-}
-
-func TestNegativeAmountTransfer(t *testing.T) {
-	ledger := mock.NewLedger(t)
-	owner := ledger.NewWallet()
-
-	ccConfig := makeBaseTokenConfig("CC Token", "CC", 8,
-		owner.Address(), "", "", owner.Address(), nil)
-	initMsg := ledger.NewCC("cc", &CustomToken{}, ccConfig)
-	require.Empty(t, initMsg)
-
-	user1 := ledger.NewWallet()
-	user2 := ledger.NewWallet()
-	user1.AddBalance("cc", 1000)
-	user2.AddBalance("cc", 500)
-
-	transferRequest := &proto.TransferRequest{
-		RequestId:       "",
-		Basis:           proto.TransferBasis_TRANSFER_BASIS_INHERITANCE,
-		AdministratorId: owner.Address(),
-		DocumentType:    proto.DocumentType_DOCUMENT_TYPE_INHERITANCE,
-		DocumentNumber:  "1",
-		DocumentDate:    timestamppb.New(time.Now()),
-		DocumentHashes: []string{
-			"hash1", "hash2",
-		},
-		FromAddress:    user1.Address(),
-		ToAddress:      user2.Address(),
-		Token:          "",
-		Amount:         "-100",
-		Reason:         "test transfer",
-		BalanceType:    proto.BalanceType_BALANCE_TYPE_TOKEN,
-		AdditionalInfo: nil,
-	}
-
-	data, err := json.Marshal(transferRequest)
-	require.NoError(t, err)
-
-	err = owner.RawSignedInvokeWithErrorReturned("cc", "transferBalance", string(data))
-	require.EqualError(t, err, "amount must be greater than zero")
-
-	user1.BalanceShouldBe("cc", 1000)
-	user2.BalanceShouldBe("cc", 500)
-}
-
-func TestZeroAmountTransfer(t *testing.T) {
-	ledger := mock.NewLedger(t)
-	owner := ledger.NewWallet()
-
-	ccConfig := makeBaseTokenConfig("CC Token", "CC", 8,
-		owner.Address(), "", "", owner.Address(), nil)
-	initMsg := ledger.NewCC("cc", &CustomToken{}, ccConfig)
-	require.Empty(t, initMsg)
-
-	user1 := ledger.NewWallet()
-	user2 := ledger.NewWallet()
-	user1.AddBalance("cc", 1000)
-	user2.AddBalance("cc", 500)
-
-	transferRequest := &proto.TransferRequest{
-		RequestId:       "",
-		Basis:           proto.TransferBasis_TRANSFER_BASIS_INHERITANCE,
-		AdministratorId: owner.Address(),
-		DocumentType:    proto.DocumentType_DOCUMENT_TYPE_INHERITANCE,
-		DocumentNumber:  "1",
-		DocumentDate:    timestamppb.New(time.Now()),
-		DocumentHashes: []string{
-			"hash1", "hash2",
-		},
-		FromAddress:    user1.Address(),
-		ToAddress:      user2.Address(),
-		Token:          "",
-		Amount:         "0",
-		Reason:         "test transfer",
-		BalanceType:    proto.BalanceType_BALANCE_TYPE_TOKEN,
-		AdditionalInfo: nil,
-	}
-
-	data, err := json.Marshal(transferRequest)
-	require.NoError(t, err)
-
-	err = owner.RawSignedInvokeWithErrorReturned("cc", "transferBalance", string(data))
-	require.EqualError(t, err, "amount must be greater than zero")
-
-	user1.BalanceShouldBe("cc", 1000)
-	user2.BalanceShouldBe("cc", 500)
 }
